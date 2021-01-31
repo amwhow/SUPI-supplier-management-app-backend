5c2274bc44fc5e22dacff119921d1e61f530e7fa
Mark <amwhow@gmail.com>
2021-01-31 16:50:04 +1100

    added controllers for models

---
+++ app/controllers/purchase_orders_controller.rb
@@ -0,0 +1,44 @@
+class PurchaseOrdersController < ApplicationController
+  def index 
+    # should be current_user.purchage_orders
+    purchase_orders = PurchaseOrder.all
+    render json: purchase_orders
+  end
+
+  def show 
+    render json: @purchase_order
+  end 
+
+  def create 
+    purchase_order = PurchaseOrder.new(purchase_orders_params)
+    # a purchase_order can engage with different users/companies
+    purchase_order.user_id = current_user.id   
+    if subscription.save
+      render status: :created 
+    else
+      render status: :bad_request
+    end
+  end
+
+  def update 
+    if @purchase_order.update(purchase_orders_params)
+      render status: :no_content 
+    else 
+      render status: :bad_request
+    end
+  end
+
+  def destroy 
+    @purchase_order.destroy
+  end
+
+  private 
+
+  def set_purchase_order 
+    @purchase_order = PurchaseOrder.find(params[:id])
+  end
+
+  def purchase_order_params 
+    params.require(:purchase_order).permit(:name, :service, :website, :contact_email, :contact_name, :contact_number, :description, :note, :user_id, :PO_document)
+  end
+end
