412f61fdcfb918bcfaae43512eb8e39dcf9cb4d1
Michael Adams <michaeladams914.dev@gmail.com>
2021-01-31 16:24:45 -0800

    updated user/supplier associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_31_230257) do
+ActiveRecord::Schema.define(version: 2021_02_01_001335) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -96,13 +96,15 @@ ActiveRecord::Schema.define(version: 2021_01_31_230257) do
     t.text "note"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_suppliers_on_user_id"
   end
 
   create_table "user_suppliers", force: :cascade do |t|
-    t.bigint "supplier_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
+    t.bigint "supplier_id", null: false
     t.index ["supplier_id"], name: "index_user_suppliers_on_supplier_id"
     t.index ["user_id"], name: "index_user_suppliers_on_user_id"
   end
@@ -122,5 +124,7 @@ ActiveRecord::Schema.define(version: 2021_01_31_230257) do
   add_foreign_key "invoices", "purchase_orders"
   add_foreign_key "purchase_orders", "suppliers"
   add_foreign_key "reviews", "purchase_orders"
+  add_foreign_key "suppliers", "users"
+  add_foreign_key "user_suppliers", "suppliers"
   add_foreign_key "user_suppliers", "users"
 end
