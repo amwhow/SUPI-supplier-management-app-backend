63d1b5b377e419d190d37c06c86aa6bcf6189baf
Michael Adams <michaeladams914.dev@gmail.com>
2021-02-06 18:40:09 -0800

    edited show action in po controller

--- app/controllers/purchase_orders_controller.rb
+++ app/controllers/purchase_orders_controller.rb
@@ -1,12 +1,19 @@
 class PurchaseOrdersController < ApplicationController
+  before_action :set_purchase_order, only: [:show, :update, :destroy]
+
   def index 
     # should be current_user.purchage_orders
     @purchase_orders = current_user.purchase_orders
+    po_files = []
+    # @purchase_orders.each do |po|
+    #   puts rails_service_blob_path(po.po_document)
+    # end
     render json: @purchase_orders
   end
 
   def show 
-    render json: @purchase_order
+    po_doc = rails_service_blob_path(@purchase_order.po_document)
+    render json: {po: @purchase_order, file: po_doc}
   end 
 
   def create 
