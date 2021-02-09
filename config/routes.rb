9c0df89ff2dd92c0c0f1e04844a1d94ca7a603b5
Mark <amwhow@gmail.com>
2021-02-09 21:13:04 +1100

    activestorage url updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  default_url_options host: 'https://localhost:3000'
+  default_url_options host: 'https://warm-peak-06763.herokuapp.com/'
   resources :suppliers
   resources :purchase_orders
   resources :invoices
