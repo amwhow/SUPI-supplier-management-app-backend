c91e25262af8e75f3a96542780c7d447177b8bfc
Mark <amwhow@gmail.com>
2021-03-22 17:36:21 +1100

    added the localhost urls

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,8 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://supi.netlify.app'
-    # origins 'localhost:3000', 'localhost:3001', '127.0.0.1:5050'
+    origins 'https://supi.netlify.app', 'localhost:3000', 'localhost:3001', 'localhost:3002'
 
     resource '*',
              headers: :any,
