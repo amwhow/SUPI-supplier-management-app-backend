2a5ec42ea8e39eb7e736ac4e78a602cae624a31f
Mark <amwhow@gmail.com>
2021-03-22 17:35:21 +1100

    formatted

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -3,13 +3,5 @@ class Supplier < ApplicationRecord
   has_many :purchase_orders, dependent: :destroy
   belongs_to :user
   has_one_attached :logo
-  # has_many_attached :documents
-
-  # validates :name, presence: true
-  # validates :name, length: { in: 2..25 }
-  # validates :service, presence: true
-  # validates :contact_name, length: { in: 2..30 }
-  # validates :contact_email, presence: true
-  # validates :description, length: { maximum: 500 }
-  # validates :note, length: { maximum: 500 }
+  
 end
